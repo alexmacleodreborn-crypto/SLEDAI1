5c388a634c4b135ef3a0f7eafde3bd7efdba34ee
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-02 18:05:54 +0000

    Create caregiverflow.py

---
+++ a7do/caregiverflow.py
@@ -0,0 +1,6 @@
+class CaregiverFlow:
+    def __init__(self, world, profiles):
+        ...
+
+    def build_day(self, day_index: int) -> list:
+        """Return ordered list of EventSpec for the day."""
