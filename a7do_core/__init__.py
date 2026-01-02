c115910ae5a673800931fff4f5a2a2c0848f0ec5
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-02 21:30:36 +0000

    Update __init__.py

--- a7do_core/__init__.py
+++ a7do_core/__init__.py
@@ -1,1 +1,7 @@
-# -- a7do ---
+# a7do_core/__init__.py
+"""
+A7DO Core Foundation
+
+This package contains the frozen architectural core.
+No learning, inference, or autonomy is implemented here.
+"""
