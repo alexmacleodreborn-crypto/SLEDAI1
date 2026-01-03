1ebfc7d167b0bc156ab7b58917dc63bd14b424af
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-03 12:15:58 +0000

    Update world_controller.py

--- world_frame/world_controller.py
+++ world_frame/world_controller.py
@@ -1,25 +1,24 @@
-from world_frame.world_state import WorldState
-from world_frame.places import hospital_birth_suite, family_home, bedroom
+from world_frame.event_generator import EventGenerator
+from world_frame.experience_controller import ExperienceController
+
 
 class WorldController:
     def __init__(self):
-        self.state = WorldState()
+        self.event_gen = EventGenerator()
+        self.experience = ExperienceController()
 
-    def initialize_birth_world(self):
-        birth_room = hospital_birth_suite()
-        self.state.places[birth_room["id"]] = birth_room
-        self.state.current_place_id = birth_room["id"]
-        self.state.current_day = 0
+    def generate_birth_experience(self):
+        """
+        High-intensity sensory burst.
+        """
+        return self.experience.birth_sequence()
 
-    def add_home_world(self):
-        home = family_home()
-        bed = bedroom()
-        self.state.places[home["id"]] = home
-        self.state.places[bed["id"]] = bed
-
-    def move_entity(self, entity_id: str, place_id: str):
-        self.state.presence[entity_id] = place_id
-        self.state.current_place_id = place_id
-
-    def advance_day(self):
-        self.state.current_day += 1
+    def generate_day_events(self, day: int, location: str, n_events: int):
+        """
+        Normal waking experiences.
+        """
+        return self.event_gen.generate(
+            day=day,
+            location=location,
+            n_events=n_events,
+        )
